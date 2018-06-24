1fdcc2f928a5013f824ee774bc4b5e17dabbbfbd
sumit241193 <sumit241193@noreply.github.com>
2018-06-25 00:46:08 +0530

    Revert "Receive changes"
    This reverts commit 9e7b2a8101ad25087c4e2bfced1edba75f05b232.

--- university/application/views/userLogin.php
+++ university/application/views/userLogin.php
@@ -1,21 +1,27 @@
 
  <?php 
 
- include('header.php');
+
+
+ require('header.php');
+
 
   ?>
  
 
 <!--=================================
  login-->
+ <style>
+ 
+ </style>
 
-<section class="page-section-ptb bg-overlay-white-70 parallax" data-jarallax='{"speed": 0.6}' style="background-image: url('/university/assets/video/video.jpg');" data-jarallax-video="mp4:/university/assets/video/video.mp4,webm:/university/assets/video/video.webm,ogv:/university/assets/video/video.ogv">
+<section class="page-section-ptb bg-overlay-white-20 parallax" data-jarallax='{"speed": 0.6}'  data-jarallax-video="mp4:/university/assets/video/video.mp4,webm:/university/assets/video/video.webm,ogv:/university/assets/video/video.ogv" style="background-image: url('/university/assets/video/video.jpg'); height:100%;">
 
   <div class="container">
-     <form action="checkuserpwd" method="post">
+     
      <div class="row">
-       <div class="col-md-6" style="margin-left:-50px; margin-top:-60px;">
-        <div class="tab tab-border icon nav-center">
+       <div class="col-md-6 col-lg-6 col-sm-6" style="margin-left:-50px;margin-top: -60px;" >
+        <div class="tab  icon nav-center">
         <ul class="nav nav-tabs">
          <li class="active"><a href="#login" data-toggle="tab"> <i class="fas fa-lock"></i>login</a></li>
          <li><a href="#signup" data-toggle="tab"> <i class="fas fa-user-plus"></i>Signup</a></li>
@@ -23,6 +29,7 @@
           <div class="tab-content">
             <div class="tab-pane fade in active" id="login">
             <div class="login-box-02 white-bg">
+			<form action="checkuserpwd" method="post">
                 <div class="pb-50 clearfix">
                    <h4 class="mb-30"> Login </h4>
                  <div class="section-field mb-20">
@@ -55,73 +62,89 @@
                   </div>
                  </div>
             </div>
-            <div class="tab-pane fade" id="signup">
-            <div class="login-box-02 white-bg">
-              <div class="pb-50 clearfix">
-                <form action="createuser" method="post">
-               <div class="row">
-                  <h4 class="mb-30">Signup</h4>
-                   <div class="section-field mb-20 col-sm-6">
-                     <label class="mb-10" for="name"></label>
-                       <input id="name" class="web form-control" type="text" placeholder="&#xf007; First Name*" style="font-family:'Poppins', sans-serif, fontawesome;" name="web">
+            <div class="tab-pane fade" id="signup" style="height: 533px;">
+            <div class="login-box-02 white-bg" style="padding:34px;">
+              <div class="pb-50 ">
+			  <form class="form-inline" action="register" method="post">
+               <div class="row ">
+                  <h4 class="mb-30" style="margin-left:50px;">Signup</h4>
+                   <div class="col-md-6 form-check">
+                     
+                       <input id="name" class="form-control" type="text" placeholder="&#xf007; First Name*" name="fname" style="font-family:'Poppins', sans-serif,FontAwesome; width:100%">
                     </div>
-                     <div class="section-field mb-20 col-sm-6">
-                     <label class="mb-10" for="name"></label>
-                       <input id="name" class="web form-control" type="text" placeholder="Last Name*" name="web" >
+                     <div class="col-md-6 form-check">
+                     
+                       <input id="name" class="web form-control" type="text" placeholder="&#xf007; Last Name*" name="lname" style="font-family:'Poppins', sans-serif,FontAwesome; width:100%">
                     </div>
                   </div>
-                  <div class="section-field mb-20 col-sm-6">
-                        MALE<input type="radio" class="form-control" name="radio" value="male">
+                 
+                  
+				 <div class="row mb-20">
+				 
+                  <div class="col-md-6 form-check ">
+				  <div  style="margin-left:165px;">
+       <i class="fas fa-male "></i> <input  name="gender" type="radio">
+        <label class="form-check-label " for="radio121">Male</label>
+    </div>
+    </div>
+
+   <div class="col-md-6 form-check ">
+				  <div  style="margin-left:-20px;">
+       <i class="fas fa-female "></i> <input  name="gender" type="radio" >
+        <label class="form-check-label " for="radio121">Female</label>
+    </div>
+    </div>
+	</div>
+					<div class="row mb-20">
+                    <div class="col-md-6 form-check ">
+                       
+                        <input type="text" placeholder="&#xf007; User Name*" class="form-control" style="font-family:'Poppins', sans-serif, fontawesome; width:100%" name="username" >
                    </div>
-                   <div class="section-field mb-20 col-sm-6">
-                        FEMALE<input type="radio" class="form-control" name="radio" value="female">
+                   <div class="col-md-6 form-check ">
+                       
+                        <input  data-provide="datepicker" class="form-control" placeholder="Date Of Birth" name="dob" style="line-height:1 !important; width:100%">
                    </div>
-                    <div class="section-field mb-20">
-                       <label class="mb-10" for="name"></label>
-                        <input type="text" placeholder="&#xf007; User Name*" class="form-control" style="font-family:'Poppins', sans-serif, fontawesome;" name="username">
+				   </div>
+                  <div class="row mb-20">
+                      <div class="col-md-12">
+                        <input type="email" placeholder="&#xf1fa; Email*" class="form-control" style="font-family:'Poppins', sans-serif, fontawesome; width:100%;" name="email">
                    </div>
-                   <div class="section-field mb-20">
-                       <label class="mb-10" for="name">Birth Date</label>
-                        <input type="date" class="form-control" placeholder="Date Of Birth" name="dob">
                    </div>
-                  <div class="section-field mb-20">
-                       <label class="mb-10" for="name"></label>
-                        <input type="email" placeholder="&#xf1fa; Email*" class="form-control" style="font-family:'Poppins', sans-serif, fontawesome;" name="email">
-                   </div>
-                  <div class="section-field mb-20">
-                   <label class="mb-10" for="Password"></label>
-                     <input id="Password" class="Password form-control" type="password" placeholder="&#xf084; Password*" style="font-family:'Poppins', sans-serif, fontawesome;" name="password">
+				   <div class="row mb-20">
+                  <div class="col-md-6 form-check">
+                   
+                     <input id="Password" class="Password form-control" type="password" placeholder="&#xf084; Password*" style="font-family:'Poppins', sans-serif, fontawesome; width:100%" name="password">
                   </div>
-                  <div class="section-field mb-20">
-                   <label class="mb-10" for="Password"> </label>
-                     <input id="Password" class="Password form-control" type="password" placeholder="&#xf084; Confirm Password*" style="font-family:'Poppins', sans-serif, fontawesome;" name="confirmpassword">
+							 
+				  
+                  <div class="col-md-6 form-check">
+                   
+					<input id="Password" class="Password form-control" type="password" placeholder="&#xf084; Confirm Password*" style="font-family:'Poppins', sans-serif, fontawesome; width:100%" name="confirmpassword">
+						  
                   </div>
-                  <div class="section-field mb-20">
-                   <label class="mb-10" for="Password"></label>
-                     <input id="text" class="Password form-control" type="password" placeholder="&#xf10b; Mobile Number*" style="font-family:'Poppins', sans-serif, fontawesome;" name="mob">
+                  </div>
+				  
+				  
+                  <div class="form-check mb-10">
+                  
+                     <input id="text" class="Password form-control" type="password" placeholder="&#xf10b; Mobile Number*" style="font-family:'Poppins', sans-serif, fontawesome; width:100%" name="mob">
                   </div>
                   <!-- Google reCaptch-->
                     <div class="g-recaptcha section-field clearfix" data-sitekey="6LdhZmAUAAAAAKcCpLcimU80gUbnvck3MQVzqyYV"  align="center"></div>
-
-                    <button type="submit" class="button btn-block">
-                            <span>Signup</span>
-                            <i class="fa fa-check"></i>
-                         </button>
+				  
+                    <a href="#" class="button btn-block">
+                      <span>Signup</span>
+                      <i class="fa fa-check"></i>
+                   </a>
+				   </form>
                 </div>
-              </form>
-                <div class="login-social text-center clearfix">
-                  <ul>
-                      <li><a class="fb" href="#"> Facebook</a></li>
-                      <li><a class="twitter" href="#"> Twitter</a></li>
-                      <li><a class="pinterest" href="#"> google+</a></li>
-                  </ul>
-                </div>
+                
                </div>
             </div> 
           </div> 
         </div>
         </div>
-		<div class="col-md-6" style="margin-top:100px;">
+		<div class="col-md-6 col-lg-6 col-sm-6" >
      
           <h4>We will help you to achieve your goals and to grow your business.</h4>
           <p class="mb-30">You will begin to realise why this </p>
@@ -135,17 +158,7 @@
 			 </div>
 			 </div>
   </div>
-          <!--<p class="mt-20">Don't have an account yet? <a href="signup-13.html"> Click to register!</a></p>
-          <div class="parallax bg-overlay-black-70 mt-30" style="background: url(/university/assets/images/login/01.jpg);">
-            <div class="pos-r pl-30 pb-30 pt-30 pr-30">
-              <h4 class="text-white">Contact Customer Support</h4>
-              <p class="text-white">If you're looking for more help or have a question to ask, please contact us.</p>
-            </div>
-          </div>-->
-        </div>
-      </div>
-  </div>
-</section>
+          </section>
 
 <!--=================================
  login-->
@@ -153,48 +166,14 @@
 
 
 
- include('footer.php');
+ require('footer.php');
 
 
   ?>
  
 
-</div>
 
-  
-
-<div id="back-to-top"><a class="top arrow" href="#top"><i class="fa fa-angle-up"></i> <span>TOP</span></a></div> 
- 
-<!--=================================
- jquery -->
-
-<!-- jquery -->
-<script type="text/javascript" src="/university/assets/js/jquery-1.12.4.min.js"></script>
-
-<!-- plugins-jquery -->
-<script type="text/javascript" src="/university/assets/js/plugins-jquery.js"></script>
-
-<!-- plugin_path -->
-<script type="text/javascript">var plugin_path = '/university/assets/js/';</script>
- 
-
-<!-- custom -->
-<script type="text/javascript" src="/university/assets/js/custom.js"></script>
-<!-- custom -->
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCNIc4LSZ1WrUf5CUpIbWt9znd0J1IFSFE&callback=myMap"></script>
-<script>
-function myMap() {
-var mapOptions = {
-    center: new google.maps.LatLng(51.5, -0.12),
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.HYBRID
-}
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
-</script>
-<script type="text/javascript" src="/university/assets/js/custom.js"></script>
 <script src='https://www.google.com/recaptcha/api.js'></script> 
-
 
 </body>
 </html>
